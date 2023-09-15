270fe1590a191c54cf157afeffeeb4e9b1c56454
INYONG KIM <ohnyong87@gmail.com>
2023-09-15 23:01:30 +0900

    feat : (Frontend Master) Add Auth samples for the testing output Viewpages on the Spring Security environment
    * Add minimum part of Authentic and Athorize sample test codes
    - This is only for the checking the viewpage works with HomeController and on the Spring Security environment
    - It needs to be refactored by the main login API server developer
    - It will be merged when The server Developer reworks

---
+++ src/main/java/com/sparta/springcafeservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.sparta.springcafeservice.repository;
+
+import com.sparta.springcafeservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByKakaoId(Long kakaoId);
+}
---
+++ src/main/java/com/sparta/springcafeservice/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sparta.springcafeservice.security;
+
+import com.sparta.springcafeservice.entity.User;
+import com.sparta.springcafeservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
