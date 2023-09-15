8776b87e69a75df3cacccfd62edfe761b424b910
INYONG KIM <ohnyong87@gmail.com>
2023-09-15 22:52:07 +0900

    feat : (Frontend Master) First init
    * Initializing and optimizing the landing view page of the base structure
    * The front-end structure is based on an open-source template and customized for the SpringBoot environment
    * JavaScript request functionality requires customization and processing such as form action connection
    * jQuery which are shooting response data to the right position on the views needs to be customized and re-defined
    * When moving to the publishing stage, static heavy images look somewhat dangerous. (Current 17.3 MB)
    -- Deployment time can increase with volume of static data
    -- Need to consider obtaining image URLs (using blogs, other service DBs, etc.) via CDNs or other detours in the near future

--- src/main/java/com/sparta/springcafeservice/controller/HomeController.java
+++ src/main/java/com/sparta/springcafeservice/controller/HomeController.java
@@ -1,4 +1,12 @@
 package com.sparta.springcafeservice.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index"; // "index"는 templates 디렉터리에 있는 HTML 템플릿 파일의 이름입니다.
+    }
 }
