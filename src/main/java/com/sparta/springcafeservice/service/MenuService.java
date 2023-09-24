4375db74af33dfd7ad6d58b39dec926f3d5b7991
INYONG KIM <ohnyong87@gmail.com>
2023-09-24 17:04:41 +0900

    refactor : AOP return applying on MenuService logic

--- src/main/java/com/sparta/springcafeservice/service/MenuService.java
+++ src/main/java/com/sparta/springcafeservice/service/MenuService.java
@@ -30,37 +30,35 @@ public class MenuService {
 
     // 메뉴 등록
     @Transactional
-    public ResponseEntity<StatusResponseDto> createMenu(MenuRequestDto menuRequestDto, User user) {
-        return handleServiceRequest(() -> {
-            //로그인 확인
-            if (user == null) {
-                throw new IllegalArgumentException("로그인 해주세요!");
-            }
+    public StatusResponseDto createMenu(MenuRequestDto menuRequestDto, User user) {
+        //로그인 확인
+        if (user == null) {
+            throw new IllegalArgumentException("로그인 해주세요!");
+        }
 
-            Menu menu = new Menu(menuRequestDto, user.getStore()); // 가게 메뉴 생성
-            List<Menu> checkMenus = menuRepository.findByMenuName(menu.getMenuName()); // 등록된 메뉴 이름으로 불러오기
-            Store userStore = user.getStore();
+        Menu menu = new Menu(menuRequestDto, user.getStore()); // 가게 메뉴 생성
+        List<Menu> checkMenus = menuRepository.findByMenuName(menu.getMenuName()); // 등록된 메뉴 이름으로 불러오기
+        Store userStore = user.getStore();
 
-            // 가게 유무 체크
-            if (userStore == null) {
-                throw new IllegalArgumentException("해당 가게는 존재하지 않습니다.");
-            }
-            // 가게 소유 여부 체크
-            if (user.getStore() == null) {
-                throw new IllegalArgumentException("사용자의 가게 정보가 없습니다.");
-            }
-            // 가게 사장인지 확인(가게의 userId와 사용자 id 비교)
-            if (!checkStoreUser(user)) {
-                throw new IllegalArgumentException("가게 사장이 아닙니다.");
-            }
-            // 중복 메뉴 이름
-            if (!checkMenus.isEmpty()) {
-                throw new IllegalArgumentException("중복된 메뉴이름입니다.");
-            }
+        // 가게 유무 체크
+        if (userStore == null) {
+            throw new IllegalArgumentException("해당 가게는 존재하지 않습니다.");
+        }
+        // 가게 소유 여부 체크
+        if (user.getStore() == null) {
+            throw new IllegalArgumentException("사용자의 가게 정보가 없습니다.");
+        }
+        // 가게 사장인지 확인(가게의 userId와 사용자 id 비교)
+        if (!checkStoreUser(user)) {
+            throw new IllegalArgumentException("가게 사장이 아닙니다.");
+        }
+        // 중복 메뉴 이름
+        if (!checkMenus.isEmpty()) {
+            throw new IllegalArgumentException("중복된 메뉴이름입니다.");
+        }
 
-            menuRepository.save(menu);
-            return new StatusResponseDto("메뉴를 등록했습니다.", 200);
-        });
+        menuRepository.save(menu);
+        return new StatusResponseDto("메뉴를 등록했습니다.", 200);
     }
 
 
@@ -88,37 +86,32 @@ public class MenuService {
 
     // 메뉴 수정
     @Transactional
-    public ResponseEntity<StatusResponseDto> updateMenu(Long id, MenuRequestDto menuRequestDto, User user) {
-        return handleServiceRequest(() -> {
+    public StatusResponseDto updateMenu(Long id, MenuRequestDto menuRequestDto, User user) {
+        Menu menu = checkMenuExist(id);
 
-            Menu menu = checkMenuExist(id);
-
-            //사용자가 사장인지 확인
-            if (!checkStoreUser(user)) {
-                throw new IllegalArgumentException("사업자가 아닙니다.");
-            } else {
-                menu.update(menuRequestDto);
-                MenuResponseDto res = new MenuResponseDto(menu);
-            }
-            return new StatusResponseDto("메뉴가 수정되었습니다.", 200);
-        });
+        //사용자가 사장인지 확인
+        if (!checkStoreUser(user)) {
+            throw new IllegalArgumentException("사업자가 아닙니다.");
+        } else {
+            menu.update(menuRequestDto);
+            MenuResponseDto res = new MenuResponseDto(menu);
+        }
+        return new StatusResponseDto("메뉴가 수정되었습니다.", 200);
     }
 
 
     // 메뉴 삭제
     @Transactional
-    public ResponseEntity<StatusResponseDto> deleteMenu(Long id, User user) {
-        return handleServiceRequest(() -> {
+    public StatusResponseDto deleteMenu(Long id, User user) {
+        Menu menu = checkMenuExist(id);
 
-            Menu menu = checkMenuExist(id);
-            // 사용자가 가게 사장인지 확인
-            if (!checkStoreUser(user)) {
-                throw new IllegalArgumentException("잘못된 접근 입니다.");
-            } else {
-                menuRepository.delete(menu);
-            }
-            return new StatusResponseDto("메뉴가 삭제되었습니다.", 200);
-        });
+        // 사용자가 가게 사장인지 확인
+        if (!checkStoreUser(user)) {
+            throw new IllegalArgumentException("잘못된 접근 입니다.");
+        } else {
+            menuRepository.delete(menu);
+        }
+        return new StatusResponseDto("메뉴가 삭제되었습니다.", 200);
     }
 
 
